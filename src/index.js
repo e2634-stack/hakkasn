524d2ea6208a41dfa8015b9a4b40a3c923645d09
e2634-stack <e2634@s.akashi.ac.jp>
2026-09-01 15:48:54 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ const INDEX_HTML = `<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<title>見守りMAP</title>
+<title>lo cat</title>
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
