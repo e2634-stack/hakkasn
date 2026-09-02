03858b78878bff180b5c907cae5887b3af8e6442
e2634-stack <e2634@s.akashi.ac.jp>
2026-09-02 09:12:26 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -337,7 +337,7 @@ body.emergency-mode h1 { color: #FFFFFF; }
     <div class="menu-btn" onclick="toggleMenu()">
         <span></span><span></span><span></span>
     </div>
-    <h1><img src="logo.png" alt="logo" class="header-logo">lo cat</h1>
+    <h1><img src="logo.png" alt="logo" class="header-logo">locat</h1>
 </header>
 
 <div id="map"></div>
