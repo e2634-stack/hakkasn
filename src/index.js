8b657269bbf98751289fd7dce878f7b4da9ad4cb
e2634-stack <e2634@s.akashi.ac.jp>
2026-09-03 12:26:12 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -213,7 +213,7 @@ header {
 }
 .modal-btn-search { background: #0288d1; color: white; }
 .modal-btn-delete { background: #e53935; color: white; }
-.modal-btn-cancel { background: #ff0000; color: white; }
+.modal-btn-cancel { background: #B2D3A5; color: white; }
 
 h1 {
     text-align: center;
